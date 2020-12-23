ac0e0df45995affc242b785ef8cced4c623afb91
vadapallyHarshitha <76416938+vadapallyHarshitha@users.noreply.github.com>
2020-12-23 12:23:54 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,28 @@
+let starRating=3;
+switch(starRating)
+{
+    case 1:
+    console.log("very bad");
+    break;
+
+    case 2:
+    console.log("averaege");
+    break;
+
+    case 3:
+        console.log("good");
+        break;
+
+    case 4:
+        console.log("very good");
+        break;
+    case 5:
+        console.log("excellent");
+        break;
+
+    default:
+        console.log("enter a valid integer between 1 and 5");
+
+        
+    
+}
